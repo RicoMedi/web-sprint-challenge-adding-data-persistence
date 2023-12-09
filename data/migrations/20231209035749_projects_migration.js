06aa4f333c4bf261eb33f3f3e984cec01e39b801
Ricardo Medina <r.medina2119@gmail.com>
2023-12-08 21:10:45 -0800

    deps and migrations built

---
+++ data/migrations/20231209035749_projects_migration.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema
+    .createTable('recipes', table =>{
+        table.increments('recipe_id')
+        table.string('recipe_name', 200).notNullable().unique()
+      })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    await knex.schema
+  .dropTableIfExists('')
+};
